f11fdc050056d99cf908544e3163c29e46ab6b28
Adhoc123 <emonhasan1010@gmail.com>
2022-11-27 14:24:22 +0600

    added form

--- src/Pages/BookingModal/BookingModal.js
+++ src/Pages/BookingModal/BookingModal.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const BookingModal = ({microbusData, electriccarData, luxurycarData}) => {
+const BookingModal = ({ microbusData, electriccarData, luxurycarData }) => {
     console.log(luxurycarData)
     return (
         <>
@@ -9,8 +9,14 @@ const BookingModal = ({microbusData, electriccarData, luxurycarData}) => {
                 <div className="modal-box relative">
                     <label htmlFor="booking-modal" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
                     <h3 className="text-lg font-bold">{microbusData?.itemName || electriccarData?.itemName || luxurycarData?.itemName}</h3>
-                  
-                    <p className="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+
+                    <form className='grid grid-cols-1 gap-4 mt-10'>
+                        <input type="text" placeholder="Type here" className="input input-bordered w-full" />
+                        <input type="text" placeholder="Type here" className="input input-bordered w-full" />
+                        <input type="text" placeholder="Type here" className="input input-bordered w-full" />
+                        <input type="text" placeholder="Type here" className="input input-bordered w-full" />
+                        <input className='btn btn-accent w-full text-white' type='submit' value='Submit' />
+                    </form>
                 </div>
             </div>
         </>
