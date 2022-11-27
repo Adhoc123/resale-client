3283d82110af06d07b9c1dbefc3f7bed99f61f7a
Adhoc123 <emonhasan1010@gmail.com>
2022-11-27 14:54:47 +0600

    added booking modal

--- src/Pages/BookingModal/BookingModal.js
+++ src/Pages/BookingModal/BookingModal.js
@@ -2,19 +2,18 @@ import React from 'react';
 
 const BookingModal = ({ microbusData, electriccarData, luxurycarData }) => {
     console.log(luxurycarData)
+    const {itemName, resalePrice} = microbusData;
     return (
         <>
             <input type="checkbox" id="booking-modal" className="modal-toggle" />
             <div className="modal">
                 <div className="modal-box relative">
                     <label htmlFor="booking-modal" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
-                    <h3 className="text-lg font-bold">{microbusData?.itemName || electriccarData?.itemName || luxurycarData?.itemName}</h3>
-
+                    <h3 className="text-lg font-bold">{itemName}</h3>
                     <form className='grid grid-cols-1 gap-4 mt-10'>
-                        <input type="text" placeholder="Type here" className="input input-bordered w-full" />
-                        <input type="text" placeholder="Type here" className="input input-bordered w-full" />
-                        <input type="text" placeholder="Type here" className="input input-bordered w-full" />
-                        <input type="text" placeholder="Type here" className="input input-bordered w-full" />
+                        <input type="text" placeholder="User Name"  className="input input-bordered w-full" />
+                        <input type="text" placeholder="Email Address" className="input input-bordered w-full" />
+                        <input type="text" defaultValue={resalePrice} disabled className="input input-bordered w-full font-bold" />
                         <input className='btn btn-accent w-full text-white' type='submit' value='Submit' />
                     </form>
                 </div>
